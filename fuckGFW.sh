b5453f104e9264fe44de838bbe676b4b2dd36cbe
Zinway Liu <zinway.liu@ucloud.cn>
2016-12-13 18:14:19 +0800

    把<nextHopIP>作为参数输入

--- fuckGFW.sh
+++ fuckGFW.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
-next_hop=X.X.X.X
+if [ $# != 1 ];then
+    echo "Usage: $0 <nextHopIP>"
+    exit 2
+elif ! [[ $1 =~ ^((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[1-9][0-9]|[0-9])\.){3}(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[1-9][0-9]|[0-9])$ ]];then
+    echo "Argument #1 <nextHopIP> must be a valid IP address!"
+    exit 3
+fi
+next_hop=$1
 
 src_dir=`dirname $(readlink -f $0)`
 raw_network_list=$src_dir/gfwlist.min.txt
